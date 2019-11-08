571721e8235a72bfb5fd291ab46b9200d05f2bd4
howardC56 <h2chang56@gmail.com>
2019-11-08 10:54:00 -0500

    alex said we only need to do half

---
+++ autoLayoutLab/autoLayoutLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autoLayoutLab
+//
+//  Created by Howard Chang on 11/7/19.
+//  Copyright © 2019 Howard Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
